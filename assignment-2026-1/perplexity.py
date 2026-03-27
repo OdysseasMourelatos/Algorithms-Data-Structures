d02524be9823fa49597c55b1b6e1d76d93b13614
Odysseas Mourelatos <odysseas.mour@gmail.com>
2026-03-27 12:48:47 +0200

    Temporarily created a function to write the necessary output to the output file

--- assignment-2026-1/perplexity.py
+++ assignment-2026-1/perplexity.py
@@ -5,23 +5,14 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 def main():
     args = parseArguments()
-    f_in = open(args.input_file)
-    f_out = open(args.out_file, 'w')
-    f_out.write("Computing perplexity for " + str(f_in.name) + "...")
-    f_out.write("\nTokenizing text...")
-    f_in.close()
-    f_out.close()
     model = createModel()
-       
-def createModel():
-    model_name = "facebook/opt-125m"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name, tie_word_embeddings=False
-    )
-    model.eval()
-    return model
-
+    f_in = open(args.input_file)
+    
+    write_file(args.out_file, f_in.name)
+    
+    f_in.close()
+    
+    
 def parseArguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file", help = "name of input file")
@@ -31,6 +22,26 @@ def parseArguments():
     parser.add_argument("--begin-context-tokens", default = 512, help = "the number of tokens that will be used as context window")
     parser.add_argument
     return parser.parse_args()
+
+def createModel():
+    model_name = "facebook/opt-125m"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, tie_word_embeddings=False
+    )
+    model.eval()
+    return model
+
+def write_file(out_file, input_file_name):
+    f_out = open(out_file, 'w')
+    f_out.write("Computing perplexity for " + str(input_file_name) + "...")
+    f_out.write("\nTokenizing text...")
+    f_out.write("\nFound " + 'x' + " tokens") # will soon be a function call
+    f_out.write("\nProcessing " + 'x' + " tokens in " + 'y' " window(s).") # will also use function 
+    for i in range(4): # 4 is temporary - just for testing purposes
+        f_out.write("\nWindow " + str(i+1) + "/4: nll = " ) 
+    f_out.write("\nPerplexity: " + 'x') # function call
+    f_out.close()
     
 if __name__ == "__main__":
     main()
