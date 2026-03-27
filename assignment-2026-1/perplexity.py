f7b11b6822d6b9dc24dbf36d65e537ba8874e554
Odysseas Mourelatos <odysseas.mour@gmail.com>
2026-03-27 12:01:16 +0200

    Imported library argparse and attempted to read positional & optional arguments

--- assignment-2026-1/perplexity.py
+++ assignment-2026-1/perplexity.py
@@ -1,7 +1,17 @@
+import argparse
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", help = "name of input file")
+    parser.add_argument("out_file", help = "name of output file")
+    parser.add_argument("--stride", default = 512, help = "the stride")
+    parser.add_argument("--n-ctx", default = 2048, help = "the size of the window")
+    parser.add_argument("--begin-context-tokens", default = 512, help = "the number of tokens that will be used as context window")
+    parser.add_argument
+    args = parser.parse_args()
+    
     model_name = "facebook/opt-125m"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(
