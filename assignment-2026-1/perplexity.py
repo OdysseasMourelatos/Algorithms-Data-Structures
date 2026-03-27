2fa9ac9c7436ad25a7011a9ab40c0002e1e47d1d
Odysseas Mourelatos <odysseas.mour@gmail.com>
2026-03-27 12:07:27 +0200

    Created functions for code clarity

--- assignment-2026-1/perplexity.py
+++ assignment-2026-1/perplexity.py
@@ -3,6 +3,18 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 def main():
+    parseArguments()
+    createModel()
+       
+def createModel():
+    model_name = "facebook/opt-125m"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, tie_word_embeddings=False
+    )
+    model.eval()
+    
+def parseArguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file", help = "name of input file")
     parser.add_argument("out_file", help = "name of output file")
@@ -11,13 +23,6 @@ def main():
     parser.add_argument("--begin-context-tokens", default = 512, help = "the number of tokens that will be used as context window")
     parser.add_argument
     args = parser.parse_args()
-    
-    model_name = "facebook/opt-125m"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name, tie_word_embeddings=False
-    )
-    model.eval()
     
 if __name__ == "__main__":
     main()
