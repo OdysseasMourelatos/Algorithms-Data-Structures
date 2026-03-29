a6bbfd72b109fe974a79410639a8c40f911850e1
Odysseas Mourelatos <odysseas.mour@gmail.com>
2026-03-29 13:12:50 +0300

    Changed funciton signature (find_windows) so that it accepts the tokens themselves, and not just the length

--- assignment-2026-1/perplexity.py
+++ assignment-2026-1/perplexity.py
@@ -8,9 +8,8 @@ def main():
     model, tokenizer = createModel()
     f_in = open(args.input_file)
     text = f_in.read()
-    
     tokens = find_tokens(tokenizer, text)
-    windows = find_windows(len(tokens), args.stride, args.n_ctx)
+    windows = find_windows(tokens, args.stride, args.n_ctx)
     
     write_file(args.out_file, f_in.name, tokens, windows)
     
@@ -23,7 +22,7 @@ def parseArguments():
     parser.add_argument("out_file", help = "name of output file")
     parser.add_argument("--stride", default = 512, help = "the stride")
     parser.add_argument("--n_ctx", default = 2048, help = "the size of the context window")
-    parser.add_argument("--begin-context-tokens", default = 512, help = "the number of tokens that will be used as initial context")
+    parser.add_argument("--begin_context_tokens", default = 512, help = "the number of tokens that will be used as initial context")
     parser.add_argument
     return parser.parse_args()
 
@@ -40,20 +39,21 @@ def find_tokens(tokenizer, text):
     tokens = tokenizer(text).input_ids
     return tokens
 
-def find_windows(tokens_length, stride, nctx):
-    windows = 1
+def find_windows(tokens, stride, nctx):
+    num_windows = 1
+    windows = []
     size = nctx
-    while size < tokens_length:
-        windows += 1
+    while size < len(tokens):
+        num_windows += 1
         size += stride
-    return windows
+    return num_windows
 
 def write_file(out_file, input_file_name, tokens, windows):
     f_out = open(out_file, 'w')
     f_out.write("Computing perplexity for " + str(input_file_name) + "...")
     f_out.write("\nTokenizing text...")
     f_out.write("\nFound " + str(len(tokens)) + " tokens") 
-    f_out.write("\nProcessing " + str(len(tokens)) + " tokens in " + str(windows) + " window(s).") # will also use function 
+    f_out.write("\nProcessing " + str(len(tokens)) + " tokens in " + str(windows) + " window(s).")
     for i in range(windows):
         f_out.write("\nWindow " + str(i+1) + "/" + str(windows) + ": nll = " ) 
     f_out.write("\nPerplexity: " + 'x') # function call
