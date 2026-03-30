96d2d301e101ae24f054b69951fd82b25f0c9929
Odysseas Mourelatos <odysseas.mour@gmail.com>
2026-03-30 10:17:27 +0300

    First attempt at finding the right indexes to evaluate

--- assignment-2026-1/perplexity.py
+++ assignment-2026-1/perplexity.py
@@ -15,7 +15,7 @@ def main():
     for window in windows:
         logits.append(get_logits(model, window))
     
-    logits_for_evaluation = find_logits_for_evaluation(logits, args.stride, args.n_ctx, args.begin_context_tokens)   
+    logits_for_evaluation = find_logits_for_evaluation(logits, len(windows), args.stride, args.n_ctx, args.begin_context_tokens)   
     write_file(args.out_file, f_in.name, tokens, len(windows))
     
     f_in.close()
@@ -72,8 +72,21 @@ def get_logits(model, window):
         logits = model(window_tensor).logits
     return logits
 
-def find_logits_for_evaluation(logits, stride, n_ctx, begin_context_tokens):
-    print()
+def find_logits_for_evaluation(logits, windows_length, stride, n_ctx, begin_context_tokens):
+    logits_for_evaluation = []
+    i = 0
+    while i < windows_length:
+        if i == 0:
+            for j in range(begin_context_tokens - 1, n_ctx):
+                logits_for_evaluation.append(j)
+        else:
+            for j in range(n_ctx + stride*(i-1) - 1, n_ctx + stride*i):
+                logits_for_evaluation.append(j)
+        i+=1
+        
+    print(logits_for_evaluation)
+    return logits_for_evaluation
+
     
 def write_file(out_file, input_file_name, tokens, windows):
     f_out = open(out_file, 'w')
