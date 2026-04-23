e3eee1e525af8308c96d685f72829a3aa1bb9908
Odysseas Mourelatos <odysseas.mour@gmail.com>
2026-04-23 18:03:45 +0300

    Added the previous work to a function so that the code looks more clear

--- assignment-2026-2/rendezvous.py
+++ assignment-2026-2/rendezvous.py
@@ -6,21 +6,25 @@ def main():
         filename = sys.argv[1]
     elif n == 3:
         filename = sys.argv[2]
-        
+    num_nodes, num_links, connections, begin_a, begin_b = get_txt_data(filename)
+    
+def get_txt_data(filename):
     f = open(filename)
     contents = list(f)
-    len(contents)
+
     num_nodes = int(contents[0][0])
     num_links = int(contents[0][2])
+    
     connections = []
     for i in range(1, len(contents) - 1):
         node_1 = int(contents[i][0])
         node_2 = int(contents[i][2])
         connections.append([node_1, node_2])
-    print(connections)
+        
     begin_a = int(contents[i+1][0])
-    begin_b = int(contents[i+1][2])
-    print(begin_a)
-    print(begin_b)
+    begin_b = int(contents[i+1][2])   
+    
+    return num_nodes, num_links, connections, begin_a, begin_b
+
 if __name__ == "__main__":
     main()
