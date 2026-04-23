88aa8a36cec3119f03db2e0a081ad21151c048cd
Odysseas Mourelatos <odysseas.mour@gmail.com>
2026-04-23 17:49:48 +0300

    Began reading the txt file and getting the contents

--- assignment-2026-2/rendezvous.py
+++ assignment-2026-2/rendezvous.py
@@ -1,14 +1,17 @@
 import sys
-import deque
-import bisect
 
 def main():
     n = len(sys.argv)
-    if n == 1:
+    if n == 2:
         filename = sys.argv[1]
-    else:
+    elif n == 3:
         filename = sys.argv[2]
-    
-    
+    i = 0
+    with open(filename) as f:
+        for line in f:
+            if i == 0:
+                num_nodes = int(line[0])
+                num_links = int(line[2])
+            i+=1
 if __name__ == "__main__":
     main()
