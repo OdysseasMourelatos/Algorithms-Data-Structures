01f61660426e81b1bc25b0119a58afa1c80c7898
Odysseas Mourelatos <odysseas.mour@gmail.com>
2026-04-23 18:01:16 +0300

    First attempt at getting the data of the graph from the txt and using that in my program

--- assignment-2026-2/rendezvous.py
+++ assignment-2026-2/rendezvous.py
@@ -6,12 +6,21 @@ def main():
         filename = sys.argv[1]
     elif n == 3:
         filename = sys.argv[2]
-    i = 0
-    with open(filename) as f:
-        for line in f:
-            if i == 0:
-                num_nodes = int(line[0])
-                num_links = int(line[2])
-            i+=1
+        
+    f = open(filename)
+    contents = list(f)
+    len(contents)
+    num_nodes = int(contents[0][0])
+    num_links = int(contents[0][2])
+    connections = []
+    for i in range(1, len(contents) - 1):
+        node_1 = int(contents[i][0])
+        node_2 = int(contents[i][2])
+        connections.append([node_1, node_2])
+    print(connections)
+    begin_a = int(contents[i+1][0])
+    begin_b = int(contents[i+1][2])
+    print(begin_a)
+    print(begin_b)
 if __name__ == "__main__":
     main()
