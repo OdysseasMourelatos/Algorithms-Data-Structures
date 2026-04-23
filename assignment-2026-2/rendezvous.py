4830665d7f277ae0bf5b534144eecdd904a15249
Odysseas Mourelatos <odysseas.mour@gmail.com>
2026-04-23 16:56:00 +0300

    Began writing rendezvous.py

---
+++ assignment-2026-2/rendezvous.py
@@ -0,0 +1,14 @@
+import sys
+import deque
+import bisect
+
+def main():
+    n = len(sys.argv)
+    if n == 1:
+        filename = sys.argv[1]
+    else:
+        filename = sys.argv[2]
+    
+    
+if __name__ == "__main__":
+    main()
