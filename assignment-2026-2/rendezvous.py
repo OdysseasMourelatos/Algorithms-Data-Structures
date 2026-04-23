26fbfceab476167656dd5427db52da551dcc2d46
Odysseas Mourelatos <odysseas.mour@gmail.com>
2026-04-23 18:42:44 +0300

    First attempt at creating the adjacency lists for each node - it works, but i'll probably improve it

--- assignment-2026-2/rendezvous.py
+++ assignment-2026-2/rendezvous.py
@@ -1,4 +1,6 @@
 import sys
+from collections import deque 
+import bisect
 
 def main():
     n = len(sys.argv)
@@ -8,10 +10,33 @@ def main():
         filename = sys.argv[2]
     num_nodes, num_links, connections, begin_a, begin_b = get_txt_data(filename)
     
+    g = {}
+    
+    for connenction in connections:
+        node_a = connenction[0]
+        node_b = connenction[1]
+        value_a = g.get(node_a)
+        value_b = g.get(node_b)
+        
+        if  value_a is None:
+            g.update({node_a: [node_b]})
+        else:
+            value_a.append(node_b)
+            g.update({node_a: value_a})
+            
+        if value_b is None:
+            g.update({node_b: [node_a]})
+        else:
+            value_b.append(node_a)
+            g.update({node_b: value_b})
+        print(g)
+        
+    
 def get_txt_data(filename):
     f = open(filename)
     contents = list(f)
-
+    f.close()
+    
     num_nodes = int(contents[0][0])
     num_links = int(contents[0][2])
     
