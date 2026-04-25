14a0e30210d8899bac3dc87c7e6783633bf2ea3f
Odysseas Mourelatos <odysseas.mour@gmail.com>
2026-04-25 18:01:43 +0300

    Seems like falsely adding elements to the lists and then removing them had too many problems, so now instead of reading the txt file line by line (which means not knowing which line is the last), i combined mr. louridas and my initial algoritthm by reading the data in a list and therefore having direct access to which line is the last

--- assignment-2026-2/rendezvous.py
+++ assignment-2026-2/rendezvous.py
@@ -12,7 +12,7 @@ def main():
         filename = sys.argv[2]
 
     g, begin_a, begin_b, nodes, links = get_graph(filename, directed)
-    print(g)
+    print(g, begin_a, begin_b, nodes, links)
     #parity_a = breadth_first_search(g, begin_a)
     #parity_b = breadth_first_search(g, begin_b)
     
@@ -21,44 +21,36 @@ def main():
 
 def get_graph(filename, directed):
     g = {}
-    first_line = True
-    with open(filename) as graph_input:
-        for line in graph_input:
-            nodes = [int(x) for x in line.split()]
-            if len(nodes) != 2:
-                continue
+    f = open(filename)
+    graph_input = list(f)
+    f.close()
+    
+    i=0
+    for line in graph_input:
+        i+=1
+        nodes = [int(x) for x in line.split()]
+        if len(nodes) != 2:
+            continue
             
-            if first_line:
-                begin_a = nodes[0]
-                begin_b = nodes[1]
-                first_line = False
-                continue
-            
-            if nodes[0] not in g:
-                g[nodes[0]] = []
-            if nodes[1] not in g:
-                g[nodes[1]] = []
-            g[nodes[0]].append(nodes[1])
-            if not directed:
-                g[nodes[1]].append(nodes[0])
+        if i == 1:
+            begin_a = nodes[0]
+            begin_b = nodes[1]
+            continue
+        elif i == len(graph_input): 
+            total_nodes = nodes[0]
+            total_links = nodes[1]
+            break
+        
+        if nodes[0] not in g:
+            g[nodes[0]] = []
+        if nodes[1] not in g:
+            g[nodes[1]] = []
+        g[nodes[0]].append(nodes[1])
+        if not directed:
+            g[nodes[1]].append(nodes[0])
                 
-            last_a = nodes[0]
-            last_b = nodes[1]
-    
-    nodes = last_a
-    links = last_b
-    
-    remove_data(g, last_a, last_b)
-    if not directed:
-        remove_data(g, last_b, last_a)
-    
-    return g, begin_a, begin_b, nodes, links
+    return g, begin_a, begin_b, total_nodes, total_links
 
-def remove_data(g, a, b):
-    for node in g.get(a):
-        if node == b:
-            g.get(a).remove(b)
-    
 def breadth_first_search(g, node):
     de = deque([])
     visited = []
