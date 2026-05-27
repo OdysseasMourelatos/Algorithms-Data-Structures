6977f932a429481dbbfdfc7b382f4c7c4506af7d
Odysseas Mourelatos <odysseas.mour@gmail.com>
2026-05-27 17:32:03 +0300

    After considerable thought of whether using a dictionary is better than a table in this particular example, i figured a table would be the better solution since i have digits in consecutive positions and would cost less (the computer wouldn't have to calculate the hash of 0 for example

--- assignment-2026-3/mathstick.py
+++ assignment-2026-3/mathstick.py
@@ -2,24 +2,24 @@ import sys, argparse, json, re
 
 def main():
     digits = create_digits_dictionary()
-    print(digits.get(0))
-    print(digits.get(2))
-    print(digits.get(0) - digits.get(2))
-    print(digits.get(2) - digits.get(0))
+    print(digits[0])
+    print(digits[2])
+    print(digits[0] - digits[2])
+    print(digits[2] - digits[0])
     
-def create_digits_dictionary():
-    digits = {
-        0 : {1,2,3,4,5,6}, 
-        1 : {2,3},
-        2 : {0,1,2,4,5},
-        3 : {0,1,2,3,4},
-        4 : {0,2,3,6},
-        5 : {0,1,3,4,6},
-        6 : {0,1,3,4,5,6},
-        7 : {1,2,3,6},
-        8 : {0,1,2,3,4,5,6},
-        9 : {0,1,2,3,4,6}
-    }
+def create_digits_table():
+    digits = [
+        {1,2,3,4,5,6}, 
+        {2,3},
+        {0,1,2,4,5},
+        {0,1,2,3,4},
+        {0,2,3,6},
+        {0,1,3,4,6},
+        {0,1,3,4,5,6},
+        {1,2,3,6},
+        {0,1,2,3,4,5,6},
+        {0,1,2,3,4,6}
+    ]
     return digits     
     
 if __name__ == "__main__":
