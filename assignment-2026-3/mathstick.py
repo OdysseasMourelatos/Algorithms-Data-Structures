697b844d3c665e1298c2d8a4326d883a145ae5b3
Odysseas Mourelatos <odysseas.mour@gmail.com>
2026-05-27 17:07:08 +0300

    Created the digits dictionary by hand as our professor suggested - next step is to create the transformation tables for each digit using smart algorithms

--- assignment-2026-3/mathstick.py
+++ assignment-2026-3/mathstick.py
@@ -1,7 +1,22 @@
 import sys, argparse, json, re
 
 def main():
-    print("hi")
+    digits = create_digits_dictionary()
+    
+def create_digits_dictionary():
+    digits = {
+        0 : {1,2,3,4,5,6}, 
+        1 : {2,3},
+        2 : {0,1,2,4,5},
+        3 : {0,1,2,3,4},
+        4 : {0,2,3,6},
+        5 : {0,1,3,4,6},
+        6 : {0,1,3,4,5,6},
+        7 : {1,2,3,6},
+        8 : {0,1,2,3,4,5,6},
+        9 : {0,1,2,3,4,6}
+    }
+    return digits     
     
 if __name__ == "__main__":
     main()
