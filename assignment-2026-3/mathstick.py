10fca4f8a3e51494ab15055fd3809d1d87f93063
Odysseas Mourelatos <odysseas.mour@gmail.com>
2026-05-27 17:52:00 +0300

    Created the transformation table for each digit succesfully

--- assignment-2026-3/mathstick.py
+++ assignment-2026-3/mathstick.py
@@ -1,26 +1,36 @@
 import sys, argparse, json, re
 
 def main():
-    digits = create_digits_dictionary()
-    print(digits[0])
-    print(digits[2])
-    print(digits[0] - digits[2])
-    print(digits[2] - digits[0])
+    digits = create_digits_table()
+    get_transformation_table(digits)
     
 def create_digits_table():
     digits = [
-        {1,2,3,4,5,6}, 
-        {2,3},
-        {0,1,2,4,5},
-        {0,1,2,3,4},
-        {0,2,3,6},
-        {0,1,3,4,6},
-        {0,1,3,4,5,6},
-        {1,2,3,6},
-        {0,1,2,3,4,5,6},
-        {0,1,2,3,4,6}
+        {1,2,3,4,5,6}, #0
+        {2,3}, #1
+        {0,1,2,4,5}, #2
+        {0,1,2,3,4}, #3
+        {0,2,3,6}, #4
+        {0,1,3,4,6}, #5
+        {0,1,3,4,5,6}, #6
+        {1,2,3,6}, #7
+        {0,1,2,3,4,5,6}, #8
+        {0,1,2,3,4,6} #9
     ]
     return digits     
+
+def get_transformation_table(digits):
+    transformation_table=[]
+    for digit1 in digits:
+        temp = []
+        for digit2 in digits:
+            added = digit2 - digit1
+            removed = digit1 - digit2
+            (a, r) = len(added), len(removed)
+            d = a - r
+            temp.append([added, removed, (a,r), d])
+        transformation_table.append(temp)
+    print(transformation_table[0][2])
     
 if __name__ == "__main__":
     main()
